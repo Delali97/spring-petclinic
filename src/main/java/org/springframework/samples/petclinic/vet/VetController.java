635895619a1c0be2842f6f13a796ea762a33a6e6
Delali97 <jennifer.kpolu@yahoo.com>
2022-10-12 21:58:32 -0300

    Update VetController.java

--- src/main/java/org/springframework/samples/petclinic/vet/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/vet/VetController.java
@@ -67,7 +67,7 @@ class VetController {
 		return vetRepository.findAll(pageable);
 	}
 
-	@GetMapping({ "/vets/{name}" })
+	@GetMapping({ "/vets" })
 	public @ResponseBody Vets showResourcesVetList() {
 		// Here we are returning an object of type 'Vets' rather than a collection of Vet
 		// objects so it is simpler for JSon/Object mapping
